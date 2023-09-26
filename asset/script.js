852ae05906d6f814d344e33dabe94242eeb5b16a
mori-akira <77237845+mori-akira@users.noreply.github.com>
2023-09-27 07:49:30 +0900

    G検定追加

--- asset/script.js
+++ asset/script.js
@@ -1,12 +1,17 @@
 $(function () {
     // 一覧アイテム押下時のスクロール
-    $('table.qualification-list > tbody > tr').each((i, e) => {
+    $('table.qualification-list > tbody > tr').each((_, e) => {
         let target = $(e).attr('data-target');
         if (target) {
             $(e).click(() => {
                 $(window).scrollTop($(`#${target}`).position().top);
             });
         }
+    });
+
+    // テーブルの各行に連番を振る
+    $('table.qualification-list>tbody>tr').each((i, e) => {
+        $($(e).children('td')[0]).text(i + 1)
     });
 
     // データテーブル適用
