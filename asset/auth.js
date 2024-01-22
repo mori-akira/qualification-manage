74802980d4376b8719f6ba9950aec1ed55750945
mori-akira <77237845+mori-akira@users.noreply.github.com>
2024-01-22 23:25:53 +0900

    Update auth.js

--- asset/auth.js
+++ asset/auth.js
@@ -1,6 +1,6 @@
-const PASS = 'UEBzc3cwcmQ=';
+const PASS = 'b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342';
 
-$(() => {
+$(async () => {
     const pass = $('#password');
     const msg = $('#passMessage');
     msg.hide();
@@ -27,11 +27,11 @@ $(() => {
         }
     });
 
-    btn.click(() => {
+    btn.click(async () => {
         if (btn.attr('disabled') == true) {
             return;
         }
-        if (btoa(pass.val()) === PASS) {
+        if (await sha256(pass.val()) === PASS) {
             const match = location.search.match(/\?target=(.+)/);
             if (!match || match.length < 2) {
                 location.assign('/index.html');
@@ -50,3 +50,9 @@ $(() => {
         }
     });
 });
+
+async function sha256(text) {
+    const uint8 = new TextEncoder().encode(text)
+    const digest = await crypto.subtle.digest('SHA-256', uint8)
+    return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2, '0')).join('')
+}
